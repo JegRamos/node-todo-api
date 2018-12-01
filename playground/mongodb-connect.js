ac4f51ebe0892e2376f0a2b2b6f42738e74cff59
Jeg Ramos <jegramos.pa@gmail.com>
2018-12-01 18:39:02 +0800

    added update

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -9,19 +9,42 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
     console.log('Connected to mongodb server');
     const db = client.db('TodoApp');
 
+    db.collection('Users').find().toArray()
+        .then((result) => {
+            console.log(JSON.stringify(result, undefined, 2));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    db.collection('Users').findOneAndUpdate({name: 'Shirley'}, {$set: {age: 41}}, {returnOriginal: false})
+        .then((result) => {
+            console.log(JSON.stringify(result, undefined, 2));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    
+    db.collection('Users').findOneAndUpdate({name: 'Jeg Ramos'}, {$set: {age: 21, name: 'Jeg Ramos'}}, {returnOriginal: false})
+        .then((result) => {
+            console.log(JSON.stringify(result, undefined, 2));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     // db.collection('Users').deleteMany({name: 'Jeg Ramos'})
     //     .then((result) => {
     //         console.log(result)
     //     }). catch((error) => {
     //         console.log('Unable to delete documents ', error);
     //     });
-    db.collection('Users').findOneAndDelete({_id: new ObjectID('5c02390ddcf0e71d87ec50fe')})
-        .then((result) => {
-            console.log('Document deleted: ');
-            console.log(result);
-        }) .catch((error) => {
-            console.log('Unable to delete document ', error);
-        });
+    // db.collection('Users').findOneAndDelete({_id: new ObjectID('5c02390ddcf0e71d87ec50fe')})
+    //     .then((result) => {
+    //         console.log('Document deleted: ');
+    //         console.log(result);
+    //     }) .catch((error) => {
+    //         console.log('Unable to delete document ', error);
+    //     });
 
     // db.collection('Todos').find({completed: true}).toArray()
     //     .then((docs) => {
