8c1543ce9ed003f82427a0e3bc9b3e9ee07185b6
Jeg Ramos <jegramos.pa@gmail.com>
2018-12-02 23:00:04 +0800

    Added POST route and refactor

---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+    username: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+});
+
+module.exports = {
+    User
+};
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/Todo');
+const { User } = require('./models/User');
+
+const app = express();
+
+//* Middlewares
+app.use(bodyParser.json());
+
+//* Routes
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+    const todo = Todo({
+        text: req.body.text
+    });
+    todo.save()
+        .then(doc => {
+            res.send(doc);
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        })
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
